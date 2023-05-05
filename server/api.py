7ae48e63496f7a4c530d1c849a6e4a53ad87850d
Nilet <nilet@tutanota.com>
2023-05-05 13:59:22 -0300

    fixing vercel thingy i guess

--- server/api.py
+++ server/api.py
@@ -1,23 +1,13 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from server.routes import router as LabRouter
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], # Adjust this to the domain of your Next.js app
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/health-check", tags=["Root"])
+@app.get("/api/health-check", tags=["Root"])
 async def healthCheck():
     return {
         "message":
         "Working as expected"
     }
 
-app.include_router(LabRouter, prefix='/lab')
+app.include_router(LabRouter, prefix='/api/lab')
