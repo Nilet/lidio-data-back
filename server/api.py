4dde450369128bb9f41bb4dc6f8b633a3206dee5
Nilet <nilet@tutanota.com>
2023-05-05 10:35:49 -0300

    added cors

--- server/api.py
+++ server/api.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from server.routes import router as LabRouter
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Adjust this to the domain of your Next.js app
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health-check", tags=["Root"])
