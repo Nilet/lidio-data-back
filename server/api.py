17dc2903f10b25d8aa567ab5deb4353a183981df
Nilet <nilet@tutanota.com>
2023-05-05 14:50:08 -0300

    testing cors again idk

--- server/api.py
+++ server/api.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from server.routes import router as LabRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+        "https://lidio-data.vercel.app/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/health-check", tags=["Root"])
 async def healthCheck():
